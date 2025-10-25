90aee9e7d5f15d620f447c334e45c3bca06a144e
Victoriarv232 <victoriaresesendv@gmail.com>
2025-10-25 14:51:34 -0300

    Ajustando app Router, componentes e serviços

---
+++ src/app/layout.tsx
@@ -0,0 +1,12 @@
+//código básico de exemplo
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="pt-BR">
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,4 @@
+//Código base de exemplo
+export default function HomePage() {
+  return <div>Página inicial</div>;
+}
